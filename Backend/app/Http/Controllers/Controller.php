71bef1ae48b1793a16b1faf45094feddfcb5f729
songviet011 <songviet011@gmail.com>
2026-04-10 05:25:07 +0700

    create model fix

--- Backend/app/Http/Controllers/Controller.php
+++ Backend/app/Http/Controllers/Controller.php
@@ -1,31 +1,8 @@
 <?php
 
-namespace App\Models;
+namespace App\Http\Controllers;
 
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Laravel\Sanctum\HasApiTokens;
-
-class Admin extends Authenticatable
+abstract class Controller
 {
-    use Notifiable,HasApiTokens;
-
-    protected $table = 'admins';
-
-    protected $fillable = [
-        'ten',
-        'email',
-        'password',
-        'is_super',
-    ];
-
-    protected $hidden = [
-        'password',
-    ];
-
-    protected $casts = [
-        'is_super' => 'boolean',
-    ];
+    //
 }
